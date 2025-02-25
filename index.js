090c709949c02a4484ec1ed8aafc0af00de6d8b8
user <olgalachowska.work@gmail.com>
2025-02-25 23:10:20 +0100

    Main Page

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const url = require("url");
 const replaceTemplate = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
 };
 
 const tempOverview = fs.readFileSync(
@@ -32,8 +42,12 @@ const server = http.createServer((req, res) => {
   if (pathName === "/" || pathName === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
-    const cardsHTML = dataObj.map((el) => replaceTemplate(tempCard, el));
-    res.end(tempOverview);
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
+    res.end(output);
 
     // Product page
   } else if (pathName === "/product") {
